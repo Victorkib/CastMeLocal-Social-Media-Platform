8d95d33523bbf9d8b49ee718ccec23f81b62c157
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-05-27 20:21:04 +0300

    updated the gitignore

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,8 +1,8 @@
 export const user = {
   _id: '64df3c064180b81adfe41d4b',
-  firstName: 'Code',
-  lastName: 'Wave',
-  email: 'codewavewithasante@gmail.com',
+  firstName: 'qin',
+  lastName: 'alexander',
+  email: 'qinalexander56@gmail.com',
   friends: [
     {
       _id: '64df3aec4180b81adfe41d32',
