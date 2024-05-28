1e740a2cf584095f18d69954c92a76a628bd6a09
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-05-28 16:18:20 +0300

    hihi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
       <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor h-screen overflow-hidden">
         <TopBar />
         <UserSLider users={users} />
-        <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
+        <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-screen">
           {/* LEFT */}
 
           <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto">
