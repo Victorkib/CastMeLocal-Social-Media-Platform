6b2ba6c9eabffc962eb14234acb4299ddce6e7c5
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-05-28 15:28:35 +0300

    viteChanges

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'https://castmelocal.onrender.com', // Update to your backend URL
