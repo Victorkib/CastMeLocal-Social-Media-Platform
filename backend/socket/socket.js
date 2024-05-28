7ed2506bdfb63c7f77bedf15ef20b92b0f6037aa
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-05-28 11:57:26 +0300

    socjket CHages

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    origin: [
+      'http://localhost:3000',
+      'https://castmelocal-frontend.onrender.com/',
+    ], // Replace with your frontend's URL
     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
