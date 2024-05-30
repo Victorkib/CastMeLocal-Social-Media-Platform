6fc312934241ed6f3bf51ab1740985616abf4202
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-05-30 16:57:18 +0300

    tadChages

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,10 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      'https://castmelocal-frontend.onrender.com',
-      'http://localhost:3000',
-    ], // Use the environment variable for frontend URL
+    origin: 'https://castmelocal-frontend.onrender.com',
+    // Use the environment variable for frontend URL
     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
